5c3ffe9a07ae49eb3469786696a75a41eb876f44
Jonathan Chasse <jonathan.r.chasse@gmail.com>
2020-07-07 20:14:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,9 @@ def oxford_comma(array)
   if array.length == 2
     array.join(" and ")
   elsif array.length >2
-    [array[0...-2].join(", "), array[-1].join(" and ")
+    array[-1].prepend "and "
+    array.join(", ")
+    # [array[0...-2].join(", "), array[-1].join(" and ")
   else
     array.join
   end
