a3c0970920a3d3dbb1fc878b61d7d71954182a0d
Jonathan Chasse <jonathan.r.chasse@gmail.com>
2020-07-07 20:02:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+  if array.length == 2
+    array.join(" and ")
+  elsif array.length >2
+    [array[0...-2].join(", "), array[-1].join(" and ")
+  else
+    array.join
+  end
 end
